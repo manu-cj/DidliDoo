d820f3c7cc2e757e516ac98665bb59588f2db16c
Antoine.C <chaponantoinepro@gmail.com>
2025-04-02 12:37:26 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import fetchEvent from "./lib/fetchEvent.js"
+import CardAllEvent from './components/cardAllEvent.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const app = document.querySelector('#app');
+const main = document.createElement('main');
+app.appendChild(main);
+
+
+await fetchEvent(`api/events/`);
+const events = await fetchEvent(`api/events/`);
+
+events.forEach(event => {
+    CardAllEvent(event)
+});
